d40a17bdd8261ec456c0c0ed885ef4fcfcab9131
Niraimathi S <niraimathisomasundaram@gmail.com>
2026-09-04 22:12:58 +0530

    Initial project setup

---
+++ test.py
@@ -0,0 +1,8 @@
+import yfinance as yf
+import pandas as pd
+import numpy as np
+
+print("FinWise AI setup is working!")
+print("yfinance imported successfully!")
+print("pandas imported successfully!")
+print("numpy imported successfully!")
